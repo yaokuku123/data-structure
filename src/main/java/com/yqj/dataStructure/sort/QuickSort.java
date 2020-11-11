156a1c5a6993660ca1eabef5c690e3ddd4625f1f
yaokuku123 <yaoqijun@outlook.com>
2020-11-11 20:03:52 +0800

    完成快速排序的功能

---
+++ src/main/java/com/yqj/dataStructure/sort/QuickSort.java
@@ -0,0 +1,87 @@
+package com.yqj.dataStructure.sort;
+
+import java.util.Arrays;
+
+/**
+ * Copyright(C),2019-2020,XXX公司
+ * FileName: QuickSort
+ * Author: yaoqijun
+ * Date: 2020/11/11 19:34
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 7, 9, 8, 3, 6};
+        System.out.println("排序前：");
+        System.out.println(Arrays.toString(arr));
+        quickSort2(arr, 0, arr.length - 1);
+        System.out.println("排序后：");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr,int low,int high){
+        int i,j,temp,t;
+        if(low>high){
+            return;
+        }
+        i=low;
+        j=high;
+        //temp就是基准位
+        temp = arr[low];
+
+        while (i<j) {
+            //先看右边，依次往左递减
+            while (temp<=arr[j]&&i<j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (temp>=arr[i]&&i<j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i<j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        //递归调用左半数组
+        quickSort(arr, low, j-1);
+        //递归调用右半数组
+        quickSort(arr, j+1, high);
+    }
+
+
+    public static void quickSort2(int[] arr, int low, int high) {
+        if (low > high) {
+            return;
+        }
+        int i = low;
+        int j = high;
+        int temp = arr[low];
+        int t = 0;
+        while (i < j) {
+            //先动j，后动i
+            while (temp<=arr[j] && i<j) {
+                j--;
+            }
+            while (temp>=arr[i]&& i<j) {
+                i++;
+            }
+            if (i < j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+
+        arr[low] = arr[i];
+        arr[i] = temp;
+
+        quickSort(arr, low, j - 1);
+        quickSort(arr, j + 1, high);
+    }
+}
