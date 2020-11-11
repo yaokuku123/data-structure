c05daf0451bf8c46525e6c231e644b47f885466f
yaokuku123 <yaoqijun@outlook.com>
2020-11-11 20:11:24 +0800

    完成插入排序的功能

--- src/main/java/com/yqj/dataStructure/sort/QuickSort.java
+++ src/main/java/com/yqj/dataStructure/sort/QuickSort.java
@@ -18,27 +18,27 @@ public class QuickSort {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void quickSort(int[] arr,int low,int high){
-        int i,j,temp,t;
-        if(low>high){
+    public static void quickSort(int[] arr, int low, int high) {
+        int i, j, temp, t;
+        if (low > high) {
             return;
         }
-        i=low;
-        j=high;
+        i = low;
+        j = high;
         //temp就是基准位
         temp = arr[low];
 
-        while (i<j) {
+        while (i < j) {
             //先看右边，依次往左递减
-            while (temp<=arr[j]&&i<j) {
+            while (temp <= arr[j] && i < j) {
                 j--;
             }
             //再看左边，依次往右递增
-            while (temp>=arr[i]&&i<j) {
+            while (temp >= arr[i] && i < j) {
                 i++;
             }
             //如果满足条件则交换
-            if (i<j) {
+            if (i < j) {
                 t = arr[j];
                 arr[j] = arr[i];
                 arr[i] = t;
@@ -49,9 +49,9 @@ public class QuickSort {
         arr[low] = arr[i];
         arr[i] = temp;
         //递归调用左半数组
-        quickSort(arr, low, j-1);
+        quickSort(arr, low, j - 1);
         //递归调用右半数组
-        quickSort(arr, j+1, high);
+        quickSort(arr, j + 1, high);
     }
 
 
@@ -65,10 +65,10 @@ public class QuickSort {
         int t = 0;
         while (i < j) {
             //先动j，后动i，若i先变化则势必会跳过第一个基准数，导致j后变化时判断出错
-            while (temp<=arr[j] && i<j) {
+            while (temp <= arr[j] && i < j) {
                 j--;
             }
-            while (temp>=arr[i]&& i<j) {
+            while (temp >= arr[i] && i < j) {
                 i++;
             }
             if (i < j) {
