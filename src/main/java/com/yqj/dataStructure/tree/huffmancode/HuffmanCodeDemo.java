915f6a6e32c08f010ecf53be58230c8689f7fbcb
yaokuku123 <yaoqijun@outlook.com>
2020-11-14 17:06:54 +0800

    实现哈夫曼编码

---
+++ src/main/java/com/yqj/dataStructure/tree/huffmancode/HuffmanCodeDemo.java
@@ -0,0 +1,10 @@
+package com.yqj.dataStructure.tree.huffmancode;
+
+/**
+ * Copyright(C),2019-2020,XXX公司
+ * FileName: HuffmanCode
+ * Author: yaoqijun
+ * Date: 2020/11/14 16:19
+ */
+public class HuffmanCodeDemo {
+}
