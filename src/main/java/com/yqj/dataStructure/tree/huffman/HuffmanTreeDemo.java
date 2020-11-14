244f97ca2702bdc415f5b5e997e9672ab8d26e7f
yaokuku123 <yaoqijun@outlook.com>
2020-11-14 13:58:55 +0800

    实现哈夫曼树的创建和遍历

--- src/main/java/com/yqj/dataStructure/tree/huffman/HuffmanTreeDemo.java
+++ src/main/java/com/yqj/dataStructure/tree/huffman/HuffmanTreeDemo.java
@@ -1,4 +1,8 @@
-package com.yqj.dataStructure.tree;
+package com.yqj.dataStructure.tree.huffman;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Copyright(C),2019-2020,XXX公司
@@ -8,6 +12,100 @@ package com.yqj.dataStructure.tree;
  */
 public class HuffmanTreeDemo {
     public static void main(String[] args) {
-        int[] arr ={13,7,8,3,29,6,1};
+        int[] arr = {13, 7, 8, 3, 29, 6, 1};
+        Node root = huffmanTree(arr);
+        preOrder(root);
+    }
+
+    public static Node huffmanTree(int[] arr){
+        List<Node> list = new ArrayList<>();
+        for (int item : arr) {
+            list.add(new Node(item));
+        }
+        while (list.size()>1){
+            //排序
+            Collections.sort(list);
+            //获取前两个最小的元素
+            Node leftNode = list.remove(0);
+            Node rightNode = list.remove(0);
+            //组合成一棵树
+            Node parent = new Node(leftNode.getValue() + rightNode.getValue());
+            //构建树并加入该新的节点
+            parent.setLeft(leftNode);
+            parent.setRight(rightNode);
+            list.add(parent);
+        }
+        return list.get(0);
+    }
+
+    public static void preOrder(Node node){
+        if (node!=null){
+            node.preOrder();
+        }else {
+            System.out.println("哈夫曼树为空无法遍历");
+        }
+    }
+
+}
+
+class Node implements Comparable<Node> {
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    //从小到大排序
+    @Override
+    public int compareTo(Node o) {
+        return this.value - o.value;
+    }
+
+    //前序遍历
+    public void preOrder() {
+        System.out.print(value + " ");
+        if (left != null) {
+            left.preOrder();
+        }
+        if (right != null) {
+            right.preOrder();
+        }
     }
 }
+
+
