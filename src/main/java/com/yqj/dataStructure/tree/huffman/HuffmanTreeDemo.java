b21ed75764b979486ec757928966452947afb472
yaokuku123 <yaoqijun@outlook.com>
2020-11-14 13:56:14 +0800

    实现哈夫曼树的创建和遍历

---
+++ src/main/java/com/yqj/dataStructure/tree/huffman/HuffmanTreeDemo.java
@@ -0,0 +1,13 @@
+package com.yqj.dataStructure.tree;
+
+/**
+ * Copyright(C),2019-2020,XXX公司
+ * FileName: HuffmanTreeDemo
+ * Author: yaoqijun
+ * Date: 2020/11/14 13:27
+ */
+public class HuffmanTreeDemo {
+    public static void main(String[] args) {
+        int[] arr ={13,7,8,3,29,6,1};
+    }
+}
